319211c43512c1e75a3306129376e4ff02b4778e
minmings111 <minmings111@gmail.com>
2026-09-04 14:44:46 +0900

    Study enum class example

---
+++ src/main/kotlin/chapter02/Color.kt
@@ -0,0 +1,20 @@
+package chapter02
+
+enum class Color(val r: Int, val g: Int, val b: Int) {
+    RED(255, 0, 0),
+    ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    INDIGO(75, 0, 255),
+    VIOLET(238, 130, 238); // MUST USE semicolon before more code.
+
+    //
+    val rgb = (r * 256 + g) * 256 + b
+    fun printColor() = println("$this is $rgb")
+}
+
+fun main() {
+    println(Color.BLUE.rgb)
+    Color.GREEN.printColor()
+}
