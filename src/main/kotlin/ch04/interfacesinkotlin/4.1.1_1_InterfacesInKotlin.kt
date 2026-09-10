21ed806e493076d49ee4f74400d727115b2c67e0
minmings111 <minmings111@gmail.com>
2026-09-10 15:26:03 +0900

    Document interface method conflict

--- src/main/kotlin/ch04/interfacesinkotlin/4.1.1_1_InterfacesInKotlin.kt
+++ src/main/kotlin/ch04/interfacesinkotlin/4.1.1_1_InterfacesInKotlin.kt
@@ -1,13 +1,39 @@
 package ch04.interfacesinkotlin
 
 interface Clickable {
-    fun click()
+    fun click() // normal method declaration
+    fun showOff() = println("I'm clickable!") // method with default body
 }
 
-class Button : Clickable {
+interface Focusable {
+    fun setFocus(b: Boolean) {
+        println("I ${if (b) "got" else "lost"} focus.")
+    }
+
+    fun showOff() {
+        println("I'm focusable!")
+    }
+}
+
+// Clickable and Focusable both have showOff().
+// Compile error:
+// class Button : Clickable, Focusable {
+//     override fun click() = println("I was clicked")
+// }
+
+// So Button must override showOff().
+class Button : Clickable, Focusable {
     override fun click() = println("I was clicked")
+
+    override fun showOff() {
+        super<Clickable>.showOff()
+        super<Focusable>.showOff()
+    }
 }
 
 fun main() {
-    Button().click()
+    val button = Button()
+    button.showOff()
+    button.setFocus(true)
+    button.click()
 }
